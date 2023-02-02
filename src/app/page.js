2395d292ea2678e665ffb09ec1b1b18f7eb245f1
Pratik <pratikl2614@gmail.com>
2023-02-02 18:30:19 +0530

    Deploy

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+"use client";
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import axios from "axios";
+import { useState } from "react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const [que, setQue] = useState("");
+  const [ans, setAns] = useState("");
+  const [currentQue, setCurrentQue] = useState("");
+  const options = {
+    method: "POST",
+    url: "https://you-chat-gpt.p.rapidapi.com/",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "d537a01898mshdab471e7421511cp1f72d5jsn642a6661fb46",
+      "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com"
+    },
+    data: `{"question":"${que}","max_response_time":30}`
+  };
+  const getData = (que) => {
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setAns(response.data)
+      setCurrentQue(que)
+      setQue('')
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        {/* <div className={styles.description}>
+          <p>
+            Get started by asking anything you wish to&nbsp;
+          </p>
+        </div> */}
+        <form className={styles.form}>
+          <input
+            type="text"
+            className={styles.form__field}
+            placeholder="Ask anything"
+            value = {que}
+            onChange={(e) => setQue(e.target.value)}
+          />
+          <button
+            type="button"
+            className={`${styles.btn} ${styles.btnPrimary} ${styles.btnInside} ${styles.uppercase}`}
+            onClick={() => getData(que)}
+          >
+            Submit
+          </button>
+        </form>
+        <br />
+        <p>{currentQue}</p>
+        <br />
+        <p className={styles.p}>{ans.answer}</p>
+      </div>
+    </main>
+  )
+}
